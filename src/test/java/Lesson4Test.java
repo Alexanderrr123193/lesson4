155065e78d69d33c1df17299c3feb52c7e27ad47
a.reshetnikov <reshetnikov.a@south.rt.ru>
2024-10-16 20:04:07 +0300

    add fix

--- src/test/java/Lesson4Test.java
+++ src/test/java/Lesson4Test.java
@@ -7,24 +7,20 @@ import static com.codeborne.selenide.CollectionCondition.texts;
 import static com.codeborne.selenide.Selenide.*;
 
 class Lesson4Test {
-    private static final String URL = "https://github.com/selenide/selenide";
 
     @BeforeAll
     static void setup() {
         Configuration.browserSize = "1920x1080";
         Configuration.pageLoadStrategy = "eager";
+        Configuration.baseUrl = "https://github.com/selenide";
     }
 
     @Test
     void testJUnit5OnWikiPage() {
-        open(URL);
+        open("/selenide");
         $("#wiki-tab").click();
-        $("ul[data-filterable-for='wiki-pages-filter'] li.Box-row.wiki-more-pages-link button")
-                .shouldBe(Condition.visible).click();
-        $$("a.Truncate-text.text-bold.py-1")
-                .findBy(Condition.text("SoftAssertions"))
-                .shouldBe(Condition.visible)
-                .click();
+        $("ul[data-filterable-for='wiki-pages-filter'] li.Box-row.wiki-more-pages-link button").shouldBe(Condition.visible).click();
+        $$("a.Truncate-text.text-bold.py-1").findBy(Condition.text("SoftAssertions")).shouldBe(Condition.visible).click();
         $("#wiki-content").shouldHave(Condition.text(
                 "@ExtendWith({SoftAssertsExtension.class})\n" +
                         "class Tests {\n" +
